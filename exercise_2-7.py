2efb41eb6a512a47394610bee7becece80e58834
DiwWir-0721 <32050642+DiwWir-0721@users.noreply.github.com>
2017-09-24 18:42:34 +0700

    Add files via upload

---
+++ exercise_2-7.py
@@ -0,0 +1,9 @@
+print("Erdiwa")
+print("\tErdiwa")
+print("Erdiwa\nWiralaga\nWirengjurit")
+print("\tErdiwa\n\tWiralaga\n\tWirengjurit")
+
+first_name = 'Erdiwa'
+first_name = first_name.rstrip()
+first_name = first_name.lstrip()
+first_name = first_name.strip()
